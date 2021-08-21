2a1ff8b439b87a3951073880f1f2daebd225aa63
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2021-08-21 13:53:35 +0000

    location permission done

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,29 +1,57 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, {useEffect, useState} from "react";
+import { Button, PermissionsAndroid, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
-import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Add browse component here</Text>
-    </View>
-  );
+  const [hasPermission, setPermission] = useState(false);
+
+  const requestLoactionPermission = async () => {
+    try {
+      const granted_fine = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+      const granted_coarse = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION);
+      if (granted_fine === PermissionsAndroid.RESULTS.GRANTED && granted_coarse === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the location");
+        setPermission(true);
+      } else {
+        console.log("Location permission denied");
+        setPermission(false);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  if(!hasPermission){
+      return (
+      <View style={styles.container}>
+        <Text style={styles.item}>Try permissions</Text>
+        <Button title="request permissions" onPress={requestCameraPermission} />
+      </View>
+    );
+      } else{
+        return (
+        <View style={styles.container}>
+        <Text style={styles.item}>We have your location hahaha</Text>
+      </View>
+        );
+      }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  item: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
+
