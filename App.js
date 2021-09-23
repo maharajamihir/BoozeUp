7e3d675ca906f08c524088eaf567f6d1bdf9698b
Franz Srambical <franz.srambical@gmail.com>
2021-09-23 12:44:51 +0200

    Refactored Authentification to Authentication

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import { Navigation } from './navigation/index.js';
-import { AuthenticationContextProvider } from './services/AuthentificationContext.js';
+import { AuthenticationContextProvider } from './services/AuthenticationContext.js';
 
 export default function App() {
 
