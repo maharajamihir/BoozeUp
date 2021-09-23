5930fbbb8e2822fefe3cf10457a8b7d418dc2432
Franz Srambical <franz.srambical@gmail.com>
2021-09-23 19:12:57 +0200

    Getting last known location (for speed)

--- navigation/index.js
+++ navigation/index.js
@@ -4,12 +4,23 @@ import { AuthenticationContext } from '../services/AuthenticationContext';
 import AppContainer from '../screens/AppContainer';
 import LoginContainer from '../screens/LoginContainer';
 
+/* TODO: revert
 export const Navigation = () => {
   const { isAuthenticated } = useContext(AuthenticationContext);
   //const isAuthenticated = true;
-    return (
-      <NavigationContainer>
-        {isAuthenticated ? <AppContainer /> : <LoginContainer />}
-      </NavigationContainer>
-    );
-  };
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppContainer /> : <LoginContainer />}
+    </NavigationContainer>
+  );
+};
+*/
+
+export const Navigation = () => {
+  //const isAuthenticated = true;
+  return (
+    <NavigationContainer>
+      <AppContainer/>
+    </NavigationContainer>
+  );
+};
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, TextInput, Text, View, Button, FlatList, SafeAreaView, TouchableOpacity} from 'react-native';
 import BoozeOffers from '../components/BoozeOffers';
+import * as Location from 'expo-location';
 
 
 
 export default function HomeScreen({ navigation }) {
-    const [userLocation, setLocation] = useState(null);
+    const [location, setLocation] = useState(null);
+    const [userLocation, setUserLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getLastKnownPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
 
     return (
       <View style={styles.container}>
+        <Text>
+          {text}
+        </Text>
         <TextInput 
             style={styles.input}
-            onChangeText={setLocation}
+            onChangeText={setUserLocation}
             placeholder="Enter your location (PLZ)"
             keyboardType="number-pad"
-        />     
+        />
+        {/* TODO: Needs fixing. I don't know about this stuff @mihir
         <BoozeOffers location={userLocation}/>
-
+         */}
       </View>
     );
   }
