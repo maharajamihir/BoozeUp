42594d1983d40a2fae394b03d3114ba032b167f0
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2021-10-08 19:16:42 +0000

    cleaning up profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthenticationContext } from "../services/AuthenticationContext";
 import { BoozeOfferContext } from "../services/BoozeOfferContext";
+import { textStyles } from "../styles/TextStyles";
 
 const ProfileScreen = () => {
 
@@ -26,10 +27,12 @@ const ProfileScreen = () => {
   } else{
     return (
       <SafeAreaView style={styles.container}>
-        <Text>Username: {userData.username}</Text>
-        <Text>Email: {userData.email}</Text>
-        <Text>Phone Number: {userData.phone_number}</Text>
-        <Text>Token: {userData.token}</Text>
+        <Text style={textStyles.paragraph}>Welcome back</Text>
+        <Text style={textStyles.title}>{userData.username}!</Text>
+        <View style={styles.box}>
+          <Text style={textStyles.paragraph}> 📧 {userData.email}</Text>
+          <Text style={textStyles.paragraph}> 📱 {userData.phone_number}</Text>
+        </View>
       </SafeAreaView>
     )
   }
@@ -40,9 +43,14 @@ const styles = StyleSheet.create({
   container:
   {
       flex: 1, 
-      justifyContent: 'center',
-      alignItems: 'center' 
+      //justifyContent: 'center',
+      //alignItems: 'center' 
+      paddingHorizontal: 20
   },
+  box:{
+    borderTopWidth: 5,
+     //borderColor: 
+  }
 });
 
 export default ProfileScreen;
---
+++ styles/TextStyles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const textStyles = StyleSheet.create({
+    title: {
+        paddingBottom: 30,
+        fontSize: 30,
+        fontWeight: "bold"
+      },
+
+      paragraph:{
+        fontSize: 20,
+      }
+
+});
