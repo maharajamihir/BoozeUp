0280142090f035aa0230a0da92f4229d749718e1
Franz Srambical <franz.srambical@gmail.com>
2021-10-09 17:10:59 +0200

    reformat & init markers

--- components/ListViewComponent.js
+++ components/ListViewComponent.js
@@ -1,7 +1,6 @@
-import React, {useState, useContext} from 'react';
-import { ActivityIndicator, StyleSheet, Text, SafeAreaView, TouchableOpacity, FlatList, RefreshControl} from 'react-native';
+import React, { useState, useContext } from 'react';
+import { ActivityIndicator, StyleSheet, Text, SafeAreaView, TouchableOpacity, FlatList, RefreshControl } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
 import { LocationContext } from '../services/LocationContext';
 import BoozeDisplay from './BoozeDisplay';
 import { textStyles } from '../styles/TextStyles';
@@ -10,89 +9,89 @@ const Item = ({ item, onPress, backgroundColor, textColor }) => (
   <TouchableOpacity onPress={onPress} style={[styles.box, backgroundColor]}>
     <Text style={[styles.booze_type, textColor]}>{item.booze_type} for {item.price}€</Text>
     <Text style={[styles.booze_type, textColor]}>Check it out now!</Text>
-   
+
   </TouchableOpacity>
 );
 
 const ListView = ({ navigation }) => {
   const [selectedId, setSelectedId] = useState(null);
-    const [boozeOffers, setboozeOffers] = useState(null);
-    const {location, error} = useContext(LocationContext);  
+  const [boozeOffers, setboozeOffers] = useState(null);
+  const { location, error } = useContext(LocationContext);
 
-    const renderItem = ({ item }) => {
-        const backgroundColor = item.id === selectedId ? "#000000" : "#ffffff";
-        const color = item.id === selectedId ? 'white' : 'black';
-    
-        return (
-          <Item
-            item={item}
-            onPress={() => {
-              setSelectedId(item.id);
-              navigation.navigate('Booze', { item: item});
-            }}
-            backgroundColor={{ backgroundColor }}
-            textColor={{ color }}
-          />
-        );
-      };
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#000000" : "#ffffff";
+    const color = item.id === selectedId ? 'white' : 'black';
 
-    const getBoozeOffers = () => {
-        const url = 'https://boozeup.herokuapp.com/browse?'
-        //const url  = 'http://localhost:5000/browse?'
-        fetch(url, {
-            method: 'POST',
-            headers: {    
-                Accept: 'application/json, text/plain, */*',
-                'Content-Type': 'application/json; charset=utf-8'
-              },  
-            body: JSON.stringify({
-                    //location : location,
-                    latitude : location.coords.latitude,
-                    longitude : location.coords.longitude,
-                })
-        }).then(response => response.json())
-        .then(data => setboozeOffers(data))
-        .then(booze => console.log("Received data for Booze List: " + JSON.stringify(boozeOffers[0])))
-        .catch(error => console.log(error))
-        .then(l => {return l});
-      }
+    return (
+      <Item
+        item={item}
+        onPress={() => {
+          setSelectedId(item.id);
+          navigation.navigate('Booze', { item: item });
+        }}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
 
-      if(location && !boozeOffers){
-        getBoozeOffers();
-    }
+  const getBoozeOffers = () => {
+    const url = 'https://boozeup.herokuapp.com/browse?'
+    //const url  = 'http://localhost:5000/browse?'
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify({
+        //location : location,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      })
+    }).then(response => response.json())
+      .then(data => setboozeOffers(data))
+      .then(booze => console.log("Received data for Booze List: " + JSON.stringify(boozeOffers[0])))
+      .catch(error => console.log(error))
+      .then(l => { return l });
+  }
 
-    const onRefresh = () => {
-      setboozeOffers(null);
-      getBoozeOffers();
-    }
+  if (location && !boozeOffers) {
+    getBoozeOffers();
+  }
 
-    if(boozeOffers){
-        return (
-        <SafeAreaView style={styles.list}>
-            <Text style={textStyles.title}>
-            Booze in Your Area:
+  const onRefresh = () => {
+    setboozeOffers(null);
+    getBoozeOffers();
+  }
+
+  if (boozeOffers) {
+    return (
+      <SafeAreaView style={styles.list}>
+        <Text style={textStyles.title}>
+          Booze in Your Area:
             </Text>
-            <FlatList
-            refreshControl={
-                <RefreshControl
-                  refreshing={!boozeOffers}
-                  onRefresh={onRefresh}
-                />
-              }
-            data={boozeOffers}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-            extraData={selectedId}
+        <FlatList
+          refreshControl={
+            <RefreshControl
+              refreshing={!boozeOffers}
+              onRefresh={onRefresh}
             />
-        </SafeAreaView>
-        );
-    } else { 
-        return (
-            <SafeAreaView style={styles.container}>
-                <ActivityIndicator size="large" color="#000000" />
-            </SafeAreaView>
-            );
-    }
+          }
+          data={boozeOffers}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={selectedId}
+        />
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator size="large" color="#000000" />
+      </SafeAreaView>
+    );
+  }
 }
 
 const Stack = createNativeStackNavigator();
@@ -106,39 +105,39 @@ const NavigableList = (
 ) => {
 
   return (
-        <Stack.Navigator>
-          <Stack.Screen
-            name="List"
-            component={ListView}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="Booze" component={BoozeDisplay} />
-        </Stack.Navigator>
-    );
+    <Stack.Navigator>
+      <Stack.Screen
+        name="List"
+        component={ListView}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Booze" component={BoozeDisplay} />
+    </Stack.Navigator>
+  );
 }
 
 export default NavigableList;
-  
+
 const styles = StyleSheet.create({
-    container:
-    {
-        flex: 1, 
-        justifyContent: 'center',
-        alignItems: 'center' 
-    },
-    box: {
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
+  container:
+  {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-    input: {
-        height: 50,
-        width: 300,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
-    list: {
-      marginBottom: 75,
-    }
+  box: {
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  input: {
+    height: 50,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  list: {
+    marginBottom: 75,
+  }
 });
--- components/MapViewComponent.js
+++ components/MapViewComponent.js
@@ -1,70 +1,100 @@
-import React, {useState, useEffect, useContext} from 'react';
-import { StyleSheet, Text, SafeAreaView, View, Dimensions} from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet, Text, SafeAreaView, View, Dimensions } from 'react-native';
 import { LocationContext } from '../services/LocationContext';
 import MapView, { Marker } from 'react-native-maps';
 
 export default function MapViewComponent({ navigation }) {
-    const {location, error} = useContext(LocationContext);  
+  const { location, error } = useContext(LocationContext);
+  const [boozeOffers, setboozeOffers] = useState(null);
 
-    let text = 'Waiting..';
-    if (error) {
-        text = error;
-    } else if (location) {
-        text = JSON.stringify(location);
-    }
+  let text = 'Waiting..';
+  if (error) {
+    text = error;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
-    return (
+const getBoozeOffers = () => {
+    const url = 'https://boozeup.herokuapp.com/browse?'
+    //const url  = 'http://localhost:5000/browse?'
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify({
+        //location : location,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      })
+    }).then(response => response.json())
+      .then(data => setboozeOffers(data))
+      .then(booze => console.log("Received data for Booze List: " + JSON.stringify(boozeOffers[0])))
+      .catch(error => console.log(error))
+      .then(l => { return l });
+  }
+
+  return (
+    <View>
+      <SafeAreaView style={styles.maincontainer}>
+      </SafeAreaView>
+
+      {location ?
         <View>
-            <SafeAreaView style={styles.maincontainer}>
-            </SafeAreaView>
-
-            {location ?
-            <View>
-                <MapView
-                style={styles.map}
-                initialRegion={{
-                    latitude: location.coords.latitude,
-                    longitude: location.coords.longitude,
-                    latitudeDelta: 0.004757,
-                    longitudeDelta: 0.006866,
-                }}
-                >
-                  <Marker
-                    coordinate={{ latitude : location.coords.latitude, longitude : location.coords.longitude }}
-                  />
-                </MapView>
-            {/*
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.004757,
+              longitudeDelta: 0.006866,
+            }}
+          >
+            <Marker
+              coordinate={{ latitude: location.coords.latitude, longitude: location.coords.longitude }}
+            />
+            {boozeOffers.map((marker) => (
+              <Marker
+                coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                //TODO: vielleicht + marker.id?
+                title={marker.booze_type}
+                description={marker.description}
+              />
+            ))}
+          </MapView>
+          {/*
                 <Text>Latitude: {location.coords.latitude}</Text>
                 <Text>Longitude: {location.coords.latitude}</Text>
 
             */}
-            </View>
-            : <Text>Loading...</Text>}
         </View>
-    );
-  }
-  
+        : <Text>Loading...</Text>}
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-    maincontainer:
-    {
-        marginTop: 100, 
-    },
-    container:
-    {
-        flex: 1,
-        marginTop: 100, 
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    input: {
-        height: 50,
-        width: 300,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-    }
+  maincontainer:
+  {
+    marginTop: 100,
+  },
+  container:
+  {
+    flex: 1,
+    marginTop: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    height: 50,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
