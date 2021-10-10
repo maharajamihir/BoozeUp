5814e5aec9eac955262f1945d210d417491b6b00
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2021-10-10 16:40:25 +0000

    added options to morescreen

---
+++ screens/MyAccount.js
@@ -0,0 +1,18 @@
+import React, {useState, useContext} from 'react';
+import {StyleSheet, Text, SafeAreaView, TouchableOpacity,ScrollView, View} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BoozeDisplay from '../components/BoozeDisplay';
+import ProfileScreen from './ProfileScreen';
+
+const MyAccount = () => {
+
+  return (
+        <Text>My Account</Text>
+    );
+}
+
+export default MyAccount;
+  
+const styles = StyleSheet.create({
+
+});
