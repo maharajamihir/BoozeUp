4d116568a75895761bcbb51f425008ae68b95007
Franz Srambical <franz.srambical@gmail.com>
2021-10-13 14:37:32 +0200

    Edited button size and placement

--- screens/LoginContainer.js
+++ screens/LoginContainer.js
@@ -6,23 +6,27 @@ import SignupScreen from './SignupScreen'
 import LoginScreen from './LoginScreen'
 import { BoozeUpButton } from "../styles/ButtonStyles";
 import { textStyles } from '../styles/TextStyles';
-import { Box, Heading, Button } from 'native-base';
+import { Box, Heading, Button, Center, NativeBaseProvider } from 'native-base';
 
 function AuthentificationScreen({ navigation }) {
 
   return (
 
-      <Box safeArea flex={1} p="2" py="8" w="90%" mx="auto">
-        <Heading size="lg" fontWeight="600" color="coolGray.800">
-          Welcome to BoozeUp!
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Box safeArea p="2" py="8" w="90%" mx="auto">
+          <Heading size="lg" fontWeight="600" color="coolGray.800">
+            Welcome to BoozeUp!
         </Heading>
-        <Button mt="2" colorScheme="indigo" _text={{ color: 'white' }} onPress={() => navigation.navigate('Login')}>
-          Login
+          <Button mt="4" py="3" colorScheme="indigo" _text={{ color: 'white' }} onPress={() => navigation.navigate('Login')}>
+            Login
         </Button>
-        <Button mt="2" colorScheme="indigo" _text={{ color: 'white' }} onPress={() => navigation.navigate('Signup')}>
-          Signup
+          <Button mt="5" py="3" colorScheme="indigo" _text={{ color: 'white' }} onPress={() => navigation.navigate('Signup')}>
+            Signup
         </Button>
-      </Box>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
 
   );
 }
