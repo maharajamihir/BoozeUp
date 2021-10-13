c29c847de8980c96dc35eeb7c9874e23aba4b90c
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2021-10-13 14:29:29 +0000

    added settings.js

---
+++ Settings.js
@@ -0,0 +1,18 @@
+import React, {useState, useContext} from 'react';
+import {StyleSheet, Text, SafeAreaView, TouchableOpacity,ScrollView, View} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BoozeDisplay from '../components/BoozeDisplay';
+import ProfileScreen from './ProfileScreen';
+
+const Settings = () => {
+
+  return (
+        <Text>Settings</Text>
+    );
+}
+
+export default Settings;
+  
+const styles = StyleSheet.create({
+
+});
